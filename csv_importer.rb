d14433d7422d6a2052eebfe5c45b3aef9304d7bd
Amanda Bittinger <abitting@email.uark.edu>
2015-10-07 18:37:03 -0500

    Added error handling for CSV importer
    -Added error handling that logs the error rows to the console and to a
    CSV called product_import_error_log, then continues importing to the
    table.
    -Changed directory location of CSV to be imported/ directory where
    error log CSV will be stored to a variable to make the script more
    modular

--- csv_importer.rb
+++ csv_importer.rb
@@ -5,6 +5,7 @@ require 'pg'
 
 $table = "product_test"
 $hostname = "localhost"
+$csvlocation = "#{Dir.home}/Documents/software_engineering/"
 $table_columns = ["description", "lookupcode", "price", "itemtype", "cost", "quantity", "reorderpoint", "restocklevel", "parentitem", "extendeddescription", "active", "msrp", "createdon"]
 $table_columns_string = $table_columns.join(",")
 connection_parameters = {
@@ -18,12 +19,17 @@ connection = PG::Connection.new(connection_parameters)
 puts connection.server_version
 connection.prepare('product_import', "insert into #{$table} (#{$table_columns_string}) values ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13)")
 
-CSV.foreach("#{Dir.home}/Documents/software_engineering/products.csv", quote_char: "|", :headers => true) do |product|
+CSV.foreach($csvlocation+"products.csv", quote_char: "|", :headers => true) do |product|
   $datecreated = product['DateCreated']
   if $datecreated == nil then 
     $datecreated = "1980-01-01 00:00:00.000"
   end
-  
-connection.exec_prepared('product_import', [product['Description'], product['ItemLookupCode'], product['Price'], product['ItemType'], product['Cost'], product['Quantity'], product['ReorderPoint'], product['RestockLevel'], product['ParentItem'], product['ExtendedDescription'], product['Inactive'], product['MSRP'], $datecreated])
+  begin
+    connection.exec_prepared('product_import', [product['Description'], product['ItemLookupCode'], product['Price'], product['ItemType'], product['Cost'], product['Quantity'], product['ReorderPoint'], product['RestockLevel'], product['ParentItem'], product['ExtendedDescription'], product['Inactive'], product['MSRP'], $datecreated])
+  rescue
+    puts $!
+    CSV.open($csvlocation+"product_import_error_log.csv", "a") do |errorRow|
+      errorRow << product
+    end
+  end
 end
-#will add functionality for error handling to complete the import
